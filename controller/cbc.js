88a6ef358157e69c290529f7a3d83ea6a8d2f247
Hoang Do <hoangdo.dh94@gmail.com>
2015-09-15 09:46:28 +0700

    Revert "log encrypt/decrypt time"
    This reverts commit 7349578a0c6a83f07d367974702018b45eb7dd84.

--- controller/cbc.js
+++ controller/cbc.js
@@ -4,61 +4,41 @@ var fs = require('fs'),
 function encode(input, output, key, iv, callback) {
     var sdes = new SDES(key);
     var lastByte = parseInt(iv, 2);
-
-    var startTime = Date.now();
-    var isDone = false;
-
     var readStream = fs.createReadStream(input);
     var writeStream = fs.createWriteStream(output);
     readStream.on('error', function(err) {
         callback(err, null);
     })
     readStream.on('end', function() {
-        isDone = true;
+    	callback(null,'OK');
     })
     readStream.on('data', function(chunk) {
         for (var i = 0; i < chunk.length; i++) {
             chunk[i] = sdes.encode(chunk[i] ^ lastByte);
             lastByte = chunk[i];
         }
-        writeStream.write(chunk, function() {
-            if (isDone) {
-                var duration = Date.now() - startTime;
-                console.log('Encrypt time', duration, 'ms');
-                callback(null, 'OK', duration);
-            }
-        });
+        writeStream.write(chunk);
     })
 }
 
 function decode(input, output, key, iv, callback) {
     var sdes = new SDES(key);
     var lastByte = parseInt(iv, 2);
-
-    var startTime = Date.now();
-    var isDone = false;
-
     var readStream = fs.createReadStream(input);
     var writeStream = fs.createWriteStream(output);
     readStream.on('error', function(err) {
         callback(err, null);
     })
     readStream.on('end', function() {
-        isDone = true;
+    	callback(null,'OK');
     })
     readStream.on('data', function(chunk) {
         for (var i = 0; i < chunk.length; i++) {
             var currByte = chunk[i];
-            chunk[i] = sdes.decode(chunk[i]) ^ lastByte;
-            lastByte = currByte;
+			chunk[i] = sdes.decode(chunk[i]) ^ lastByte;
+			lastByte = currByte;
         }
-        writeStream.write(chunk, function() {
-            if (isDone) {
-                var duration = Date.now() - startTime;
-                console.log('Decrypt time', duration, 'ms');
-                callback(null, 'OK', duration);
-            }
-        });
+        writeStream.write(chunk);
     })
 }
 
