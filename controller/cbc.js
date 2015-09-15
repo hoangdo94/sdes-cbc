7349578a0c6a83f07d367974702018b45eb7dd84
Hoang Do <hoangdo.dh94@gmail.com>
2015-09-15 09:37:33 +0700

    log encrypt/decrypt time

--- controller/cbc.js
+++ controller/cbc.js
@@ -4,41 +4,61 @@ var fs = require('fs'),
 function encode(input, output, key, iv, callback) {
     var sdes = new SDES(key);
     var lastByte = parseInt(iv, 2);
+
+    var startTime = Date.now();
+    var isDone = false;
+
     var readStream = fs.createReadStream(input);
     var writeStream = fs.createWriteStream(output);
     readStream.on('error', function(err) {
         callback(err, null);
     })
     readStream.on('end', function() {
-    	callback(null,'OK');
+        isDone = true;
     })
     readStream.on('data', function(chunk) {
         for (var i = 0; i < chunk.length; i++) {
             chunk[i] = sdes.encode(chunk[i] ^ lastByte);
             lastByte = chunk[i];
         }
-        writeStream.write(chunk);
+        writeStream.write(chunk, function() {
+            if (isDone) {
+                var duration = Date.now() - startTime;
+                console.log('Encrypt time', duration, 'ms');
+                callback(null, 'OK', duration);
+            }
+        });
     })
 }
 
 function decode(input, output, key, iv, callback) {
     var sdes = new SDES(key);
     var lastByte = parseInt(iv, 2);
+
+    var startTime = Date.now();
+    var isDone = false;
+
     var readStream = fs.createReadStream(input);
     var writeStream = fs.createWriteStream(output);
     readStream.on('error', function(err) {
         callback(err, null);
     })
     readStream.on('end', function() {
-    	callback(null,'OK');
+        isDone = true;
     })
     readStream.on('data', function(chunk) {
         for (var i = 0; i < chunk.length; i++) {
             var currByte = chunk[i];
-			chunk[i] = sdes.decode(chunk[i]) ^ lastByte;
-			lastByte = currByte;
+            chunk[i] = sdes.decode(chunk[i]) ^ lastByte;
+            lastByte = currByte;
         }
-        writeStream.write(chunk);
+        writeStream.write(chunk, function() {
+            if (isDone) {
+                var duration = Date.now() - startTime;
+                console.log('Decrypt time', duration, 'ms');
+                callback(null, 'OK', duration);
+            }
+        });
     })
 }
 
