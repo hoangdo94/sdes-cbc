a38930210b1b224e4d6e89970969d6a259098cdd
Hoang Do <hoangdo.dh94@gmail.com>
2015-09-15 10:38:29 +0700

    progress icon

--- public/javascripts/decrypt.js
+++ public/javascripts/decrypt.js
@@ -2,16 +2,17 @@ $('.header li a[href="/decrypt"]').parent().addClass('pure-menu-selected');
 $('form').submit(function(e) {
     e.preventDefault();
     //disable all input
-    $('#file').prop('disabled', true);
-    $('#key').prop('disabled', true);
-    $('#iv').prop('disabled', true);
+    $('form input').prop('disabled', true);
     $('#download-link').hide();
+    $('#up').attr('class', '');
+    $('#dp').attr('class', '');
     //show progress
     $('#progress-container').show();
     changeProgress($('#upload-progress'), 0);
     changeProgress($('#decrypt-progress'), 0);
     //upload file
     var file = document.getElementById('file').files[0];
+    $('#up').attr('class', 'fa fa-spinner fa-spin');
     uploadFile(file, function(percent) {
         changeProgress($('#upload-progress'), percent);
     }, function(status, r) {
@@ -24,14 +25,16 @@ $('form').submit(function(e) {
                 filename: r.name,
                 filepath: r.path,
             }
+            $('#up').attr('class', 'fa fa-check');
+            $('#dp').attr('class', 'fa fa-spinner fa-spin');
             $.post('/decrypt', data, function(r) {
-                $('#file').prop('disabled', false);
-                $('#key').prop('disabled', false);
-                $('#iv').prop('disabled', false);
+                $('form input').prop('disabled', false);
                 if (r === 'failed') {
+                    $('#dp').attr('class', 'fa fa-times');
                     alert('Failed...');
                 } else {
                     changeProgress($('#decrypt-progress'),100);
+                    $('#dp').attr('class', 'fa fa-check');
                     $('#download-link').show();
                     $('#download-link').attr('href', r);
                     // alert('Done...');
